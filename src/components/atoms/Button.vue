e5dbfcf43f11ede03c46db8647c4975185cedd02
Kazushi Konosu <kazushi@konosu.me>
2019-04-24 14:14:17 +0900

    add button

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -1,7 +1,43 @@
 <template lang="pug">
-	button テキスト
+  div
+    button.button(
+      :class="{ 'button--primary': $props.color === 'primary' }"
+      @click="$emit('click')"
+    )
+      slot
 </template>
 
 <script>
-export default {}
+import VueTypes from 'vue-types'
+
+export default {
+  props: {
+    color: VueTypes.oneOf(['primary', 'normal']).def('normal')
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.button {
+  background-color: #eee;
+  color: #333;
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  position: relative;
+  padding: 8px 16px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  margin: 0;
+
+  &--primary {
+    background-color: #0070e8;
+    color: #fff;
+  }
+
+  &:active {
+    opacity: 0.4;
+  }
+}
+</style>
--- stories/atoms.js
+++ stories/atoms.js
@@ -1,9 +1,32 @@
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, select, text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 import { Button } from '../src/components/atoms'
 
-storiesOf('atoms', module).add('Button', () => ({
+const stories = storiesOf('atoms', module)
+stories.addDecorator(withKnobs)
+
+stories.add('Button', () => ({
   components: {
     Button
   },
-  template: `<Button />`
+  props: {
+    name: {
+      default: text('name', '名前')
+    },
+    color: {
+      default: select('color', ['normal', 'primary'], 'normal')
+    }
+  },
+  methods: {
+    onClick() {
+      action('clicked')()
+    }
+  },
+  template: `
+    <Button
+      :color="$props.color"
+      @click="onClick"
+    >{{ $props.name }}</Button>
+  `
 }))
