405ebb955a91825baaaab737d0751e3caa89625b
Kazushi Konosu <kazushi@konosu.me>
2019-04-24 21:44:28 +0900

    refactor

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,23 @@
+// atoms
 import Button from './atoms/Button.vue'
 import Input from './atoms/Input.vue'
 import ChatBubble from './atoms/ChatBubble.vue'
 import UserImage from './atoms/UserImage.vue'
 
+// molecules
 import MessageListItem from './molecules/MessageListItem.vue'
 import MessageInput from './molecules/MessageInput.vue'
 
+// organisms
 import Header from './organisms/Header.vue'
+
+// pages
+import Home from './pages/Home.vue'
 
 export const atoms = { Button, Input, ChatBubble, UserImage }
 
 export const molecules = { MessageListItem, MessageInput }
 
 export const organisms = { Header }
+
+export const pages = { Home }
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from './pages/Home.vue'
+import { pages } from './components'
+
+const { Home } = pages
 
 Vue.use(Router)
 
