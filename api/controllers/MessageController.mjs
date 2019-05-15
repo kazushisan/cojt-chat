40659e3b423ef71434e98b5365851f5542cc9f39
Kazushi Konosu <kazushi@konosu.me>
2019-05-15 14:38:03 +0900

    init message create controller

--- api/controllers/MessageController.mjs
+++ api/controllers/MessageController.mjs
@@ -2,6 +2,10 @@ class MessageController {
   read(req, res) {
     res.status(200).json({ message: 'hello world' })
   }
+
+  create(req, res) {
+    res.status(200).json({ message: 'successfully created' })
+  }
 }
 
 const messageController = new MessageController()
