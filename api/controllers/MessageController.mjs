b7ff2e8c27c527de43320f0b02e4d39977710686
Kazushi Konosu <kazushi@konosu.me>
2019-05-15 14:50:56 +0900

    wip: add function to create message

--- api/controllers/MessageController.mjs
+++ api/controllers/MessageController.mjs
@@ -1,9 +1,16 @@
+import MessageModel from '../models/MessageModel'
+
 class MessageController {
   read(req, res) {
     res.status(200).json({ message: 'hello world' })
   }
 
   create(req, res) {
+    const { content, connection } = req.body
+    const from = req.login._id
+    const message = new MessageModel({
+      from: 
+    })
     res.status(200).json({ message: 'successfully created' })
   }
 }
--- api/models/UserModel.mjs
+++ api/models/UserModel.mjs
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose'
 
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  displayName: String,
-  name: String,
-  password: String,
-  mail: String
-})
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    displayName: String,
+    name: String,
+    password: String,
+    mail: String
+  },
+  {
+    timestamps: true
+  }
+)
 
 const UserModel = mongoose.model('User', userSchema)
 
