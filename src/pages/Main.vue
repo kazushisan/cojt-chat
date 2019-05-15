a902ca6c51a33e4faa3a67c462eb9e6596b76969
Kazushi Konosu <kazushi@konosu.me>
2019-05-15 12:43:34 +0900

    wip main screen

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,9 +1,33 @@
 <template lang="pug">
-  
+  div
+    Header
+    .main
+      .main__inner
+      
 </template>
 
 <script>
-export default {}
+import { organisms } from '../components'
+// import api from '../services/api'
+
+const { Header } = organisms
+
+export default {
+  components: {
+    Header
+  },
+  data() {
+    return {
+      connections: [
+        {
+          _id: 'a',
+          name: 'グループ1',
+          users: ['a', 'b']
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
